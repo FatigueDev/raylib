18791cbe3376a2dedc95d3e96b3c9287c7f8e764
Fatigue <fatigue.dev@gmail.com>
2025-04-11 14:54:33 +1000

    It didn't.

--- src/memory.h
+++ src/memory.h
@@ -2,20 +2,21 @@
 
 #include <erl_nif.h>
 
-void* calloc_wrap(int n, size_t sz) {
-    size_t total_size = n * sz;
-    void* ptr = enif_alloc(total_size);
-    if (ptr) {
-        memset(ptr, 0, total_size);
-    }
+#ifndef CALLOC_WRAP
+#define CALLOC_WRAP(n,sz)\
+    size_t total_size = n * sz;\
+    void* ptr = enif_alloc(total_size);\
+    if (ptr) {\
+        memset(ptr, 0, total_size);\
+    }\
     return ptr;
-}
+#endif
 
 #ifndef RL_MALLOC
     #define RL_MALLOC(sz)       enif_alloc(sz)
 #endif
 #ifndef RL_CALLOC
-    #define RL_CALLOC(n,sz)     calloc_wrap(n, sz)
+    #define RL_CALLOC(n,sz)     CALLOC_WRAP(n, sz)
 #endif
 #ifndef RL_REALLOC
     #define RL_REALLOC(ptr,sz)  enif_realloc(ptr,sz)
