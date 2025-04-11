fcf4e9f4ddc2180e816788f7fadb261e9954bbdb
Fatigue <fatigue.dev@gmail.com>
2025-04-11 14:20:19 +1000

    Attempt to un-bugger the memory allocation (Cheers, Jairo)

--- src/raylib.h
+++ src/raylib.h
@@ -133,7 +133,15 @@
     #define RL_MALLOC(sz)       enif_alloc(sz)
 #endif
 #ifndef RL_CALLOC
-    #define RL_CALLOC(n,sz)     enif_alloc(n*sz)
+    void* calloc_wrap(int n, size_t sz) {
+        size_t total_size = n * sz;
+        void* ptr = enif_alloc(total_size);
+        if (ptr) {
+            memset(ptr, 0, total_size);
+        }
+        return ptr;
+    }
+    #define RL_CALLOC(n,sz)     calloc_wrap(n,sz)
 #endif
 #ifndef RL_REALLOC
     #define RL_REALLOC(ptr,sz)  enif_realloc(ptr,sz)
