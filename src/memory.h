a8ceab6b9da9784aabdc95e687ad14875c92f3ed
Fatigue <fatigue.dev@gmail.com>
2025-04-11 14:57:55 +1000

    Copy pasting code at this point. Beyond flabberghasted.

--- src/memory.h
+++ src/memory.h
@@ -2,25 +2,36 @@
 
 #include <erl_nif.h>
 
-#ifndef CALLOC_WRAP
-#define CALLOC_WRAP(n,sz)\
-    size_t total_size = n * sz;\
-    void* ptr = enif_alloc(total_size);\
-    if (ptr) {\
-        memset(ptr, 0, total_size);\
-    }\
+void* nif_alloc(size_t size) {
+    return enif_alloc(size);
+}
+
+void* nif_calloc(size_t num, size_t size) {
+    size_t total_size = num * size;
+    void* ptr = enif_alloc(total_size);
+    if (ptr) {
+        memset(ptr, 0, total_size);
+    }
     return ptr;
-#endif
+}
+
+void* nif_realloc(void* ptr, size_t new_size) {
+    return enif_realloc(ptr, new_size);
+}
+
+void nif_free(void* ptr) {
+    enif_free(ptr);
+}
 
 #ifndef RL_MALLOC
-    #define RL_MALLOC(sz)       enif_alloc(sz)
+    #define RL_MALLOC(sz)       nif_alloc(sz)
 #endif
 #ifndef RL_CALLOC
-    #define RL_CALLOC(n,sz)     CALLOC_WRAP(n, sz)
+    #define RL_CALLOC(n,sz)     nif_calloc(n, sz)
 #endif
 #ifndef RL_REALLOC
-    #define RL_REALLOC(ptr,sz)  enif_realloc(ptr,sz)
+    #define RL_REALLOC(ptr,sz)  nif_realloc(ptr,sz)
 #endif
 #ifndef RL_FREE
-    #define RL_FREE(ptr)        enif_free(ptr)
+    #define RL_FREE(ptr)        nif_free(ptr)
 #endif
