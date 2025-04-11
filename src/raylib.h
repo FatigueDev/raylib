b3641c030271bb653d6000b3215b204512b69385
Fatigue <fatigue.dev@gmail.com>
2025-04-11 14:32:26 +1000

    Redefining the macro

--- src/raylib.h
+++ src/raylib.h
@@ -128,20 +128,23 @@
 #endif
 
 #include <erl_nif.h>
-    
+
+#ifndef CALLOC_WRAP
+    #define CALLOC_WRAP(n, sz) (\
+        size_t total_size = n * sz;\
+        void* ptr = enif_alloc(total_size);\
+        if (ptr) {\
+            memset(ptr, 0, total_size);\
+        }\
+        return ptr;\
+    )
+#endif
+
 #ifndef RL_MALLOC
     #define RL_MALLOC(sz)       enif_alloc(sz)
 #endif
 #ifndef RL_CALLOC
-    void* calloc_wrap(int n, size_t sz) {
-        size_t total_size = n * sz;
-        void* ptr = enif_alloc(total_size);
-        if (ptr) {
-            memset(ptr, 0, total_size);
-        }
-        return ptr;
-    }
-    #define RL_CALLOC(n,sz)     calloc_wrap(n,sz)
+    #define RL_CALLOC(n,sz) CALLOC_WRAP(n, sz)
 #endif
 #ifndef RL_REALLOC
     #define RL_REALLOC(ptr,sz)  enif_realloc(ptr,sz)
